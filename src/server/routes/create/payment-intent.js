26f52787a39814d54dd4c1bc118842f97e191e94
Jonathan Steele <jonathan@steele.pro>
2021-01-26 22:54:28 +0000

    fix: Correct constant name

--- src/server/routes/create/payment-intent.js
+++ src/server/routes/create/payment-intent.js
@@ -4,9 +4,9 @@ export default async function createPaymentIntent(req, res, options) {
   try {
     const stripe = new Stripe(options.secret_key)
 
-    const session = await stripe.paymentIntents.create(req.body)
+    const paymentIntent = await stripe.paymentIntents.create(req.body)
 
-    res.status(201).json(session)
+    res.status(201).json(paymentIntent)
   } catch ({ statusCode, raw: { message } }) {
     res.status(statusCode).json({ message, status: statusCode })
   }
