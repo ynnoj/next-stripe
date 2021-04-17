9af37f0622901f28885232d15fb45f0001a09851
Nisharg Shah <nishargshah3101@gmail.com>
2021-04-18 02:45:04 +0530

    added stripe key intelligence in server main file

--- index.ts
+++ index.ts
@@ -1,12 +1,13 @@
-import { INextRequest, INextResponse } from './src/types/types'
+import { INextRequest, INextResponse, IOptions } from './src/types/types'
 import { NextStripeHandler } from './src/server'
 
-export default function NextStripe(...args: any) {
+export default function NextStripe(
+  ...args: [INextRequest, INextResponse, IOptions] | [IOptions]
+) {
   if (args.length === 1) {
     return (req: INextRequest, res: INextResponse) =>
       NextStripeHandler(req, res, args[0])
   }
 
-  // @ts-ignore
   return NextStripeHandler(...args)
 }
